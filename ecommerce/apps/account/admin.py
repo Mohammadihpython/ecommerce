12206e8a8009b85b2995dff3ca6aa45686e214d5
Mohammadihpython <mohammadih87@yahoo.com>
2023-11-26 10:41:09 +0330

    change staticfiles directory

--- ecommerce/apps/account/admin.py
+++ ecommerce/apps/account/admin.py
@@ -8,5 +8,5 @@ from .models import User
 class UserAdmin(admin.ModelAdmin):
     list_display = ["first_name", "last_name", "phone_number", "date_joined"]
     list_filter = ["date_joined"]
-    ordering = ["-date_created"]
+    ordering = ["-date_joined"]
     search_fields = ["phone_number", "last_name", "first_name"]
