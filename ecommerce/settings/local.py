81c456279c9e009ed49f1e1b481e74fdf0090374
Mohammadihpython <mohammadih87@yahoo.com>
2023-11-27 17:43:45 +0330

    add model images diagram

--- ecommerce/settings/local.py
+++ ecommerce/settings/local.py
@@ -26,4 +26,4 @@ if env("USE_DOCKER") == "yes":
 # django-extensions
 # ------------------------------------------------------------------------------
 # https://django-extensions.readthedocs.io/en/latest/installation_instructions.html#configuration
-# INSTALLED_APPS += ["django_extensions"]  # noqa: F405
+INSTALLED_APPS += ["django_extensions"]  # noqa: F405
