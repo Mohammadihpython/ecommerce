d923168abb6f81ddb05744acc7391285563ab8a3
Mohammadihpython <mohammadih87@yahoo.com>
2023-12-03 19:39:10 +0330

    :))

--- ecommerce/apps/product/tests/test_product_models.py
+++ ecommerce/apps/product/tests/test_product_models.py
@@ -43,5 +43,5 @@ def test_product_attribute_value_model(
 @pytest.mark.django_db
 def test_product_inventory_model(db, product_factory, product_inventory_factory):
     # product = product_factory.create()
-    inventory_product = product_inventory_factory()
+    inventory_product = product_inventory_factory.create()
     assert str(inventory_product) == inventory_product.product.name
